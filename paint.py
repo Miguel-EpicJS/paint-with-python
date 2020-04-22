3c6163396abda34a9afeace2c40143e4d393f578
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2020-04-22 08:37:57 -0300

    Update paint.py

--- paint.py
+++ paint.py
@@ -11,38 +11,30 @@ b = 0
 
 windown = pygame.display.set_mode((1500,1500))
 
-pygame.display.set_caption("Carrinho")
+
+
+pygame.display.set_caption("Paint")
 
 clock = pygame.time.Clock()
 
 windown_open = True
 pygame.font.init()
 
+txt = "Color".format(r,g,b)
 
+fonte=pygame.font.get_default_font()
+fontesys=pygame.font.SysFont(fonte, 60)
 
 
 while windown_open:
+    pygame.display.flip()
 
-    txtR = 'Red:  '
-    txtG = 'Green: '
-    txtB = 'Blue: '
-    numR = '{}'.format(r)
-    numG = '{}'.format(g)
-    numB = '{}'.format(b)
-    fonte=pygame.font.get_default_font()
-    fontesys=pygame.font.SysFont(fonte, 60)
-    txttelaR = fontesys.render(txtR, 1, (255,255,255))
-    txttelaG = fontesys.render(txtG, 1, (255,255,255))
-    txttelaB = fontesys.render(txtB, 1, (255,255,255))
-    numtelaR = fontesys.render(numR, 1, (255,255,255))
-    numtelaG = fontesys.render(numG, 1, (255,255,255))
-    numtelaB = fontesys.render(numB, 1, (255,255,255))
-    windown.blit(txttelaR,(0,0))
-    windown.blit(txttelaG,(0,50))
-    windown.blit(txttelaB,(0,100))
-    windown.blit(numtelaR,(150,0))
-    windown.blit(numtelaG,(150,50))
-    windown.blit(numtelaB,(150,100))
+
+
+    txttela = fontesys.render(txt, 1, (r,g,b))
+    windown.blit(txttela,(0,0))
+
+
 
     mouse = pygame.mouse.get_pos()
     comand = pygame.key.get_pressed()
@@ -53,28 +45,30 @@ while windown_open:
             windown_open = False
 
     if event.type == pygame.MOUSEBUTTONDOWN:
-        pygame.draw.circle(windown, (r,g,b), (mouse),25)
+        pygame.draw.circle(windown, (r,g,b), (mouse),30)
 
     if comand[pygame.K_F1] and r < 255:
         r+=1
-        numtelaR = fontesys.render(numR, 1, (255,255,255))
+
     if comand[pygame.K_F2] and g < 255:
         g+=1
+
 
     if comand[pygame.K_F3] and b < 255:
         b+=1
 
+
     if comand[pygame.K_F4] and r > 1:
         r-=1
 
+
     if comand[pygame.K_F5] and r > 1:
         g-=1
+
 
     if comand[pygame.K_F6] and r > 1:
         b-=1
 
 
-
-
-    pygame.display.update()
+    #pygame.display.update()
 pygame.quit()
